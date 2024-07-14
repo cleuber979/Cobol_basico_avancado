648357c4d0d037452e783010176701e70d35c7d8
Cleuber Maciel <105437987+cleuber979@users.noreply.github.com>
2024-07-13 23:42:19 -0300

    Add files via upload

---
+++ teste1.cbl
@@ -0,0 +1,21 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. PROGRAMA01.
+      ******************************************************************
+      * Author: Cleuber Maciel
+      * Date:08/07/24
+      * Purpose: Learn Cobol
+      * Tectonics: cobc
+      ******************************************************************
+       ENVIRONMENT DIVISION.
+       CONFIGURATION SECTION.
+       INPUT-OUTPUT SECTION.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            DISPLAY "Hello world"
+            DISPLAY "BEM VINDO AO PRIMEIRO PROGRAMA EM COBOL"
+            DISPLAY  "VAMOS APRENDER?"
+            STOP RUN.
+       END PROGRAM PROGRAMA01.
