12db53e0cbe2742fa93de365c8b7cd68caa1ba6e
Cleuber Maciel <105437987+cleuber979@users.noreply.github.com>
2024-07-29 10:39:55 -0300

    Primeiro programa de leitura de arquivo BD cobol
    Este primero programa tem passo a passo para se lê um arquivo externo no cobol a qual ele pega uma informação e guarda numa variável e mostra o resultado.

---
+++ ARQ001.cob
@@ -0,0 +1,42 @@
+       PROGRAM-ID. ARQ001.
+      ******************************************************************
+      * Author:CLEUBER MACIEL
+      * Date: 29/07/2024
+      * Purpose: ABERTURA DO ARQUIVO CLIENTES.DAT
+      * Tectonics: cobc
+      ******************************************************************
+       ENVIRONMENT DIVISION.
+       CONFIGURATION SECTION.
+       INPUT-OUTPUT SECTION.
+       FILE-CONTROL.
+           SELECT CLIENTES ASSIGN TO "C:\cobol\DADOS\CLIENTES.DAT.txt"
+           FILE STATUS IS FS-CLIENTES.
+
+       DATA DIVISION.
+       FILE SECTION.
+       FD  CLIENTES.
+       01  REG-CLIENTES.
+           05 REG-ID        PIC 9(04).
+           05 REG-NOME      PIC X(20).
+           05 REG-TELEFONE  PIC X(11).
+       WORKING-STORAGE SECTION.
+       77  FS-CLIENTES PIC 9(02).
+
+       PROCEDURE DIVISION.
+       0000-PRINCIPAL         SECTION.
+           PERFORM 0100-INICIALIZAR.
+           PERFORM 0300-FINALIZAR.
+           STOP RUN.
+       0100-INICIALIZAR       SECTION.
+           OPEN INPUT CLIENTES.
+           IF FS-CLIENTES EQUAL 35
+               DISPLAY"ARQUIVO NAO FOI ABERTO "
+           ELSE
+           DISPLAY "ARQUIVO OK ".
+
+       0200-PROCESSAR         SECTION.
+           CONTINUE.
+       0300-FINALIZAR         SECTION.
+
+           DISPLAY "Hello world".
+           CLOSE CLIENTES.
